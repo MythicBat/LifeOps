868ffaab73a7eed6bd98ed8faf4c28ac51a88217
Alin Merchant <alinmerchant5@gmail.com>
2026-08-16 20:53:20 +1000

    updating auto actions

--- lifeops/agent/lifeops/guardian.py
+++ lifeops/agent/lifeops/guardian.py
@@ -18,6 +18,8 @@ AUTO_ACTIONS = {
     "archive_receipt",
     "track_subscription",
     "track_warranty",
+    "track_renewal",
+    "add_appointment",
     "create_decision",
     "no_action",
 }
